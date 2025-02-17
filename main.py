af8dcd215404b627d4f7ce7c8aca1f4e6962a537
Nickkcj <nickcj123@hotmail.com>
2025-02-16 22:24:16 -0300

    Fix: bugs + keep scores/change bg

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
+print(question_bank)
+
 
 quiz = QuizBrain(question_bank)
-quiz_ui = QuizInterface()
+quiz_ui = QuizInterface(quiz)
 
 
-# while quiz.still_has_questions():
-#     quiz.next_question()
+while quiz.still_has_questions():
+    quiz.next_question()
 
-print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+quiz_ui.end_quiz()
